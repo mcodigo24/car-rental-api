7f1527f1e67ae3af2f9de681e12aa3d87cfceac1
MatiDev <mcodigo24@gmail.com>
2025-04-03 02:04:43 -0300

    TSK005 - Add IsCarAvailableAsync service.

--- car-rental-api/Domain/Repositories/ICarsRepository.cs
+++ car-rental-api/Domain/Repositories/ICarsRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace car_rental_api.Domain.Repositories
+﻿using car_rental_api.Domain.Entities;
+
+namespace car_rental_api.Domain.Repositories
 {
     public interface ICarsRepository
     {
+        Task<Car?> GetCarWithRentalsAndServicesAsync(int carId);
     }
 }
